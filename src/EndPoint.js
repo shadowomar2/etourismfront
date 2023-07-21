5177cd612a56215ae5e83c7ae3a3456334c9bc58
shadowomar2 <hamshoitsolution@gmail.com>
2023-07-21 12:48:15 +0300

    deploy website2

--- src/EndPoint.js
+++ src/EndPoint.js
@@ -1,5 +1,5 @@
 const baseURL = "http://91.144.22.143:7778";
-const baseURLLocally = "http://localhost:8081";
+const baseURLLocally = "example-omar-etourism.onrender.com";
 export const Imagefitch_URL = "/test/download/";
 export const BACKEND_URL = baseURLLocally;
 
--- src/axios_URL.js
+++ src/axios_URL.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081";
+axios.defaults.baseURL = "https://example-omar-etourism.onrender.com";
 axios.defaults.headers.post["Content-type"] = "application/json";
 
  
